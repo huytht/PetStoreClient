aae11cc4f9fd72be6d4e412fd74516d632926f47
huytht <huytht2001@gmail.com>
2022-07-15 19:48:57 +0700

    update checkout form

--- src/components/Checkout/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm.js
@@ -1,87 +1,356 @@
-import React from 'react'
-import "./style.css"
-import {FaCcPaypal,FaShoppingCart} from 'react-icons/fa'
+import React from "react";
+import "./style.css";
+import { FaShoppingCart, FaPaypal, FaMoneyBillWave } from "react-icons/fa";
+import { Input, Row, Col, Container, Radio, Checkbox } from "@nextui-org/react";
 const CheckoutForm = () => {
+  const [checked, setChecked] = React.useState(false);
+
   return (
     <>
-        <h1 className='title-checkout mtop'>Thanh toán</h1>
-        <div className="box-checkout-form">
-  <div class="box-main-checkout">
-    <div class="container-checkout">
-      <form >
-      
-        <div className="box-checkout-form">
-          <div className="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input className='input-checkout-form'  type="text" id="fname" name="firstname" placeholder="John M. Doe"/>
-            <label  for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input className='input-checkout-form' type="text" id="email" name="email" placeholder="john@example.com"/>
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input className='input-checkout-form' type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input className='input-checkout-form' type="text" id="city" name="city" placeholder="New York"/>
+      <h1 className="title-checkout mtop">Thanh toán</h1>
+      <div className="box-checkout-form">
+        <div class="box-main-checkout">
+          <div class="container-checkout">
+            <form>
+              <div className="box-checkout-form">
+                <h2 className="title">
+                  <i class="fa-solid fa-address-card"></i> Thông tin liên hệ
+                </h2>
+                <Container>
+                  <Row gap={1} className="checkout-element-row">
+                    <Col className="box-checkout-element">
+                      <label for="lastName">
+                        <i class="fa fa-user"></i> Họ và tên đệm
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                    <Col className="box-checkout-element">
+                      <label for="firstName">
+                        <i class="fa fa-user"></i> Tên
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                  </Row>
+                  <Row gap={1}>
+                    <Col className="box-checkout-element">
+                      <label for="email">
+                        <i class="fa fa-envelope"></i> Email
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                    <Col className="box-checkout-element">
+                      <label for="email">
+                        <i class="fa fa-phone"></i> Số điện thoại
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                  </Row>
+                </Container>
 
-            <div className="row">
-              <div className="col-50">
-                <label for="state">State</label>
-                <input className='input-checkout-form' type="text" id="state" name="state" placeholder="NY"/>
+                <h2 className="title">
+                  <i class="fa-solid fa-truck"></i> Thông tin giao hàng
+                </h2>
+                <Container>
+                  <Row gap={1} className="checkout-element-row">
+                    <Col className="box-checkout-element">
+                      <label for="lastName">
+                        <i class="fa-solid fa-city"></i> Tỉnh/Thành phố
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                    <Col className="box-checkout-element">
+                      <label for="firstName">
+                        <i class="fa-solid fa-city"></i> Quận/Huyện
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                  </Row>
+                  <Row gap={1}>
+                    <Col className="box-checkout-element">
+                      <label for="email">
+                        <i class="fa-solid fa-city"></i> Phường/Xã
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                    <Col className="box-checkout-element">
+                      <label for="email">
+                        <i class="fa-solid fa-location-dot"></i> Địa chỉ chi
+                        tiết
+                      </label>
+                      <Input
+                        // clearable
+                        // value={lastName}
+                        // onChange={(e) => setLastName(e.target.value)}
+                        helperColor="error"
+                        // helperText={lastNameToched && helperLastName.text}
+                        // onFocus={() => setLastNameToched(true)}
+                        type="text"
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="md"
+                      />
+                    </Col>
+                  </Row>
+                </Container>
+                <h2 className="title">
+                  <i class="fa-solid fa-truck-fast"></i> Phương thức giao hàng
+                </h2>
+                <Container >
+                  <Row gap={1} css={{ boxShadow: '0 0 0 1px', borderRadius: '0.375em', padding: '1.125rem' }} className="checkout-element-row">
+                    <Col className="box-checkout-element">
+                      <Radio.Group defaultValue="A">
+                        <Radio value="A">Giao hàng nhanh (1-3 ngày)</Radio>
+                      </Radio.Group>
+                    </Col>
+                    <Col
+                      css={{ textAlign: "right", fontWeight: "bold", fontSize: 18 }}
+                      className="box-checkout-element"
+                    >
+                      Miễn phí
+                    </Col>
+                  </Row>
+                </Container>
+                <h2 className="title">
+                  <i class="fa-solid fa-money-bill-1"></i> Thông tin thanh toán
+                </h2>
+                <br />
+                <Checkbox
+                  disableAnimation={true}
+                  className="checkout-checkbox"
+                  value={checked}
+                  onChange={() => setChecked(!checked)}
+                  defaultSelected={true}
+                >
+                  Thông tin thanh toán giống thông tin giao hàng
+                </Checkbox>
+                {checked && (
+                  <Container>
+                    <Row gap={1} className="checkout-element-row">
+                      <Col className="box-checkout-element">
+                        <label for="lastName">
+                          <i class="fa-solid fa-city"></i> Tỉnh/Thành phố
+                        </label>
+                        <Input
+                          // clearable
+                          // value={lastName}
+                          // onChange={(e) => setLastName(e.target.value)}
+                          helperColor="error"
+                          // helperText={lastNameToched && helperLastName.text}
+                          // onFocus={() => setLastNameToched(true)}
+                          type="text"
+                          bordered
+                          fullWidth
+                          color="primary"
+                          size="md"
+                        />
+                      </Col>
+                      <Col className="box-checkout-element">
+                        <label for="firstName">
+                          <i class="fa-solid fa-city"></i> Quận/Huyện
+                        </label>
+                        <Input
+                          // clearable
+                          // value={lastName}
+                          // onChange={(e) => setLastName(e.target.value)}
+                          helperColor="error"
+                          // helperText={lastNameToched && helperLastName.text}
+                          // onFocus={() => setLastNameToched(true)}
+                          type="text"
+                          bordered
+                          fullWidth
+                          color="primary"
+                          size="md"
+                        />
+                      </Col>
+                    </Row>
+                    <Row gap={1}>
+                      <Col className="box-checkout-element">
+                        <label for="email">
+                          <i class="fa-solid fa-city"></i> Phường/Xã
+                        </label>
+                        <Input
+                          // clearable
+                          // value={lastName}
+                          // onChange={(e) => setLastName(e.target.value)}
+                          helperColor="error"
+                          // helperText={lastNameToched && helperLastName.text}
+                          // onFocus={() => setLastNameToched(true)}
+                          type="text"
+                          bordered
+                          fullWidth
+                          color="primary"
+                          size="md"
+                        />
+                      </Col>
+                      <Col className="box-checkout-element">
+                        <label for="email">
+                          <i class="fa-solid fa-location-dot"></i> Địa chỉ chi
+                          tiết
+                        </label>
+                        <Input
+                          // clearable
+                          // value={lastName}
+                          // onChange={(e) => setLastName(e.target.value)}
+                          helperColor="error"
+                          // helperText={lastNameToched && helperLastName.text}
+                          // onFocus={() => setLastNameToched(true)}
+                          type="text"
+                          bordered
+                          fullWidth
+                          color="primary"
+                          size="md"
+                        />
+                      </Col>
+                    </Row>
+                  </Container>
+                )}
+                <h2 className="title">
+                <FaMoneyBillWave /> Hình thức thanh toán
+                </h2>
+                <Container>
+                  <Row gap={1} className="checkout-element-row">
+                    <Col className="box-checkout-element">
+                      <Radio.Group css={{ lineHeight: 3 }} defaultValue="COD">
+                        <Radio className="checkbox-item" value="COD"><img class="method-icon" src="https://frontend.tikicdn.com/_desktop-next/static/img/icons/checkout/icon-payment-method-cod.svg" width="32" height="32" alt="icon" />
+                          &nbsp; Thanh toán bằng tiền mặt
+                        </Radio>
+                        <Radio className="checkbox-item" value="Paypal"><FaPaypal style={{ color: "#00457C" }}/>&nbsp;&nbsp; Thanh toán bằng Paypal</Radio>
+                        <Radio value="Momo">
+                          <img class="method-icon" src="https://frontend.tikicdn.com/_desktop-next/static/img/icons/checkout/icon-payment-method-momo.svg" width="32" height="32" alt="icon" /> 
+                          &nbsp;Thanh toán bằng ví MoMo
+                        </Radio>
+                        </Radio.Group>
+                      </Col>
+                  </Row>
+                </Container>
               </div>
-              <div className="col-50">
-                <label for="zip">Zip</label>
-                <input className='input-checkout-form' type="text" id="zip" name="zip" placeholder="10001"/>
-              </div>
-            </div>
+              <input
+                className="btn-checkout-form"
+                type="submit"
+                value="Continue to checkout"
+              />
+            </form>
           </div>
-
-          <div className="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-             <FaCcPaypal/>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input className='input-checkout-form' type="text" id="cname" name="cardname" placeholder="John More Doe"/>
-            <label for="ccnum">Credit card number</label>
-            <input className='input-checkout-form' type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
-            <label for="expmonth">Exp Month</label>
-            <input className='input-checkout-form' type="text" id="expmonth" name="expmonth" placeholder="September"/>
-            <div className="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input className='input-checkout-form' type="text" id="expyear" name="expyear" placeholder="2018"/>
-              </div>
-              <div className="col-50">
-                <label for="cvv">CVV</label>
-                <input className='input-checkout-form' type="text" id="cvv" name="cvv" placeholder="352"/>
-              </div>
-            </div>
-          </div>
-          
         </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"/> Shipping address same as billing
-        </label>
-        <input className='btn-checkout-form' type="submit" value="Continue to checkout"/>
-      </form>
-    </div>
-  </div>
-  <div className="col-25">
-    <div className="container-checkout">
-      <h4>Cart <span class="header-price"><FaShoppingCart/> <b>4</b></span></h4>
-      <br/>
-      <p><a href="#">Product 1</a> <span class="price">$1</span></p>
-      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
-      <br/>
-      <hr/>
-      <p>Total <span className="price"><b>$30</b></span></p>
-    </div>
-  </div>
-</div>
-    </>      
-  )
-}
+        <div className="col-25">
+          <div className="container-checkout">
+            <h4>
+              Cart{" "}
+              <span class="header-price">
+                <FaShoppingCart /> <b>4</b>
+              </span>
+            </h4>
+            <br />
+            <p>
+              <a href="#">Product 1</a> <span class="price">$1</span>
+            </p>
+            <p>
+              <a href="#">Product 2</a> <span class="price">$5</span>
+            </p>
+            <p>
+              <a href="#">Product 3</a> <span class="price">$8</span>
+            </p>
+            <p>
+              <a href="#">Product 4</a> <span class="price">$2</span>
+            </p>
+            <br />
+            <hr />
+            <p>
+              Total{" "}
+              <span className="price">
+                <b>$30</b>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CheckoutForm
+export default CheckoutForm;

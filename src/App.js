7062b8ee3f2adca45fb7e3177318111e2a2a64fc
khava00 <81426052+khava00@users.noreply.github.com>
2022-07-17 01:36:05 +0700

    Update Tawk.to
    Fix re-render when refresh page

--- src/App.js
+++ src/App.js
@@ -13,20 +13,6 @@ import CheckoutForm from "./components/Checkout/CheckoutForm";
 
 
 const App = () => {
-  useEffect(() => {
-    var Tawk_API = Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    (function () {
-      var s1 = document.createElement("script"),
-        s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src='https://embed.tawk.to/62d18b897b967b117999b8bf/1g818i4eo';
-      s1.charset = "UTF-8";
-      s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-  }, []);
- 
   return (
     <>
       <Toaster

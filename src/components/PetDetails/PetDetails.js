a1a30c1c08dcf97371994b6528f5188b33c59751
khava00 <81426052+khava00@users.noreply.github.com>
2022-08-01 23:15:45 +0700

    update url

--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -61,7 +61,7 @@ const PetDetails = () => {
       <Helmet>
         <title>{product.name}</title>
         <meta property='og:title' content={product.name}/>
-        <meta property='og:url' content={ `http://38.242.132.213:3000/product/${convertURL(product?.name)}-${product.id}`}/>
+        <meta property='og:url' content={ `http://38.242.132.213:3001/product/${convertURL(product?.name)}-${product.id}`}/>
         <meta property='og:image' content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
         <meta property='og:description' content={product.description}/>
       </Helmet>
